f30af97fddd04aea3f9e5987f5064b2a2ab04ccb
258356 <80736939+258356@users.noreply.github.com>
2021-04-22 23:38:24 +0530

    ifelse4update

--- python_programming_submission/ifelse_4.py
+++ python_programming_submission/ifelse_4.py
@@ -1,1 +1,30 @@
+input(real,float,string,complex,zero)
 
+def check_float(potential_float):
+    try:
+        float(potential_float)
+        return True
+    except ValueError:
+        return False
+    
+def check_int(potential_float):
+    try:
+        int(potential_float)
+        return True
+    except ValueError:
+        return False
+
+value=input("enter a number")
+
+if(check_int(value)):
+    if(int(value)==0):
+        print("zero")
+    else:
+        print("Integer")
+
+else:
+    if(check_float(value)):
+        print("Float")
+
+    else:
+        print("Complex")
