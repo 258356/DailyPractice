8ef6a6b7dfd707d953253295d0718c131f054c1a
258356 <80736939+258356@users.noreply.github.com>
2021-04-22 23:33:25 +0530

    ifelseupdated

--- python_programming_submission/ifelse_2.py
+++ python_programming_submission/ifelse_2.py
@@ -1,1 +1,12 @@
 
+string = input()
+if string == "lol":
+    print("laughing out loud")
+elif string == "rofl":
+    print("rolling on the floor laughingd")
+elif string == "lmk":
+    print("let me know")
+elif string == "smh":
+    print( "shaking my head")
+else:
+    print("error")
